2aa03922f09f4a1c916972c293f773cb6386d407
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-09-08 12:21:47 -0500

    Implementing the UserController update method

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -67,7 +67,45 @@ class UserController extends Controller
   */
   public function update(Request $request, $id)
   {
-    //
+    $user = User::findOrFail($id);
+
+    $reglas = [
+      'email' => 'email|unique:users,email,' . $user->id,
+      'password' => 'min:6|confirmed',
+      'admin' => 'in:' . User::ADMIN_USER . ',' . User::REGULAR_USER,
+    ];
+
+    $this->validate($request, $reglas);
+
+    if ($request->has('name')) {
+      $user->name = $request->name;
+    }
+
+    if ($request->has('email') && $user->email != $request->email) {
+      $user->verified = User::UNVERIFIED_USER;
+      $user->verification_token = User::generarVerificationToken();
+      $user->email = $request->email;
+    }
+
+    if ($request->has('password')) {
+      $user->password = bcrypt($request->password);
+    }
+
+    if ($request->has('admin')) {
+      if (!$user->isVerified()) {
+        return response()->json(['error' => 'Unicamente los usuarios verificados pueden cambiar su valor de administrador', 'code' => 409], 409);
+      }
+      $user->admin = $request->admin;
+    }
+
+    if (!$user->isDirty()) {
+      return response()->json(['error' => 'Se debe especificar al menos un valor diferente para actualizar', 'code' => 422], 422);
+    }
+
+    $user->save();
+
+    return response()->json(['data' => $user], 200);
+
   }
 
   /**
