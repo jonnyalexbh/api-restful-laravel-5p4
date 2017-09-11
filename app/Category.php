e0b957aa1192652b13f8116b0bd7d3a2aedf9520
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-09-11 09:46:47 -0500

    linking the models with its respective transformer

--- app/Category.php
+++ app/Category.php
@@ -4,26 +4,25 @@ namespace App;
 
 use App\Product;
 use Illuminate\Database\Eloquent\Model;
+use App\Transformers\CategoryTransformer;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-	use SoftDeletes;
+  use SoftDeletes;
 
-	protected $dates = ['deleted_at'];
+  public $transformer = CategoryTransformer::class;
+  protected $dates = ['deleted_at'];
+  protected $fillable = [
+    'name',
+    'description',
+  ];
+  protected $hidden = [
+    'pivot'
+  ];
 
-	protected $fillable = [
-		'name',
-		'description',
-	];
-	protected $hidden = [
-		'pivot'
-	];
-
-	// many-to-many relationship
-	public function products()
-	{
-		return $this->belongsToMany(Product::class);
-	}
-
+  public function products()
+  {
+    return $this->belongsToMany(Product::class);
+  }
 }
