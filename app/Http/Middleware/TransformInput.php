67724062e3025012e6f94e1e35eac4cc98e92cb8
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-09-11 12:35:57 -0500

    solving the first problem using the middleware

--- app/Http/Middleware/TransformInput.php
+++ app/Http/Middleware/TransformInput.php
@@ -6,15 +6,23 @@ use Closure;
 
 class TransformInput
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next($request);
+  /**
+  * Handle an incoming request.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  \Closure  $next
+  * @return mixed
+  */
+  public function handle($request, Closure $next, $transformer)
+  {
+    $transformedInput = [];
+
+    foreach ($request->request->all() as $input => $value) {
+      $transformedInput[$transformer::originalAttribute($input)] = $value;
     }
+
+    $request->replace($transformedInput);
+
+    return $next($request);
+  }
 }
