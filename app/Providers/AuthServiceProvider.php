61efa67ac8e286ee5f1db27516dc309ef333a09e
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-09-29 08:22:24 -0500

    registering some required scopes for the API

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -9,27 +9,35 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+  /**
+  * The policy mappings for the application.
+  *
+  * @var array
+  */
+  protected $policies = [
+    'App\Model' => 'App\Policies\ModelPolicy',
+  ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+  /**
+  * Register any authentication / authorization services.
+  *
+  * @return void
+  */
+  public function boot()
+  {
+    $this->registerPolicies();
 
-        Passport::routes();
-        Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
-        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
-        Passport::enableImplicitGrant();
-    }
+    Passport::routes();
+    Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
+    Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+    Passport::enableImplicitGrant();
+
+    Passport::tokensCan([
+      'purchase-product' => 'Create a new transaction for a specific product',
+      'manage-products' =>  'Create, reade, update, and delete products (CRUD)',
+      'manage-account' => 'Read your account data, id, name, email, if verified, and if admin (cannot read password). Modify your account data (email, and password). Cannot delete your account',
+      'read-general' => 'Read general information like purchasing categories, purchased products, selling products, selling categories, your transactions (purchases and sales)',
+    ]);
+
+  }
 }
